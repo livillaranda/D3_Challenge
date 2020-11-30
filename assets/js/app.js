d8d762c45ce5cd0330cc1295ee83b7c7748a338c
livillaranda <livillaranda@gmail.com>
2020-11-30 15:14:05 -0500

    Homework completed. Attempting bonus sections.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,101 @@
-// @TODO: YOUR CODE HERE!
+// Define SVG Dimensions
+var svgWidth = 1000;
+var svgHeight = 800;
+
+// Chart Margins as Object
+var margin = {
+    top: 100,
+    bottom: 100,
+    left: 100,
+    right: 100
+};
+
+// Define Chart Dimensions
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// SVG Wrapper
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append SVG Group
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Load Data
+d3.csv("assets/data/data.csv").then(function (dabblerData) {
+    
+    // View Data in Console
+    console.log(dabblerData);
+
+    // Parse Data from CSV
+    dabblerData.forEach(data => {
+        data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
+    });
+
+    // Create X-Axis Scale Function
+    var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(dabblerData, d => d.obesity)])
+        .range([0, chartWidth]);
+    
+    // Create Y-Axis Scale Function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(dabblerData, d => d.healthcare)])
+        .range([chartHeight, 0]);
+    
+    // Create Axis Functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to Chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+    
+    chartGroup.append("g")
+        .call(leftAxis);
+    
+    // Create Circle Data Points
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(dabblerData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.obesity))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .attr("fill", "lightblue")
+    
+    // Add Abbreviations in Circles
+    chartGroup.selectAll("null")
+        .data(dabblerData)
+        .enter()
+        .append("text")
+        .text(function (x) { return x.abbr })
+        .attr("x", d => xLinearScale(d.obesity))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("text-anchor", "middle")
+        .attr("dy", 5)
+        .attr("font-size", 15)
+        .attr("fill", "white")
+        .attr("font-weight", "bold")
+    
+    // Axes Labels
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top - 10})`)
+        .attr("class", "axisText")
+        .text("Obesity (%)");
+    
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Healthcare (% with)");
+    
+}).catch(function (error) {
+    console.log(error);
+});
